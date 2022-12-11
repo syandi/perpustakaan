0b6b9921be1c9d287cbac3ece6802a095225a750
syandi <sandianugrah96@gmail.com>
2022-12-11 12:36:53 +0700

    logo

--- resources/views/admin-lte/sidebar.blade.php
+++ resources/views/admin-lte/sidebar.blade.php
@@ -1,7 +1,7 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
-      <img src="/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('image/logo-mts.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">{{config('app.name')}}</span>
     </a>
 
