e86f839b5ef81c3b78a7d00c3b5ddfaf0bdaa30b
syandi <sandianugrah96@gmail.com>
2022-12-24 10:23:32 +0700

    path

--- resources/views/admin-lte/javascript.blade.php
+++ resources/views/admin-lte/javascript.blade.php
@@ -1,6 +1,6 @@
 <!-- jQuery -->
-<script src="/adminlte/plugins/jquery/jquery.min.js"></script>
+<script src="/perpustakaan/public/adminlte/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/perpustakaan/public/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/adminlte/dist/js/adminlte.js"></script>
+<script src="/perpustakaan/public/adminlte/dist/js/adminlte.js"></script>
--- resources/views/admin-lte/script/chart.blade.php
+++ resources/views/admin-lte/script/chart.blade.php
@@ -1,2 +1,2 @@
 <!-- ChartJS -->
-<script src="/adminlte/plugins/chart.js/Chart.min.js"></script>
+<script src="/perpustakaan/public/adminlte/plugins/chart.js/Chart.min.js"></script>
